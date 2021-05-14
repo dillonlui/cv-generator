3a22e0af046a840a04c53492676335c70c612238
Dillon Lui <dillonglui@gmail.com>
2021-05-14 16:28:11 -0500

    remove save button and combine name inputs

--- src/components/General.js
+++ src/components/General.js
@@ -1,29 +1,21 @@
 import React from "react";
-import GeneralPreview from "./GeneralPreview"
 
-class General extends React.Component {
+export default class General extends React.Component {
     constructor(){
         super()
         this.state = {
-            firstName: '',
-            lastName: '',
+            name: '',
             location: '',
             email: '',
             phone: '',
             website: '',
-            isEdit: true
         }
         this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(e){
         const { id, value} = e.target
         this.setState({[id]: value})
-    }
-    handleSubmit(e){
-        e.preventDefault()
-        this.setState( {isEdit: !this.state.isEdit})
     }
 
     render(){
@@ -33,54 +25,41 @@ class General extends React.Component {
             <hr />
             <div className="inputForm">
                 <div>
-                <form onSubmit={this.handleSubmit} style={{display: this.state.isEdit ? 'block' : 'none'}}>
-                <input 
-                    type="text" 
-                    id="firstName" 
-                    placeholder="First Name" 
-                    value={this.state.firstName}
-                    onChange={this.handleChange}/>
-                <input 
-                    type="text" 
-                    id="lastName" 
-                    placeholder="Last Name" 
-                    value={this.state.lastName}
-                    onChange={this.handleChange}/>
-                <input 
-                    type="text" 
-                    id="location" 
-                    placeholder="Location" 
-                    value={this.state.location}
-                    onChange={this.handleChange}/>
-                <input 
-                    type="email" 
-                    id="email" 
-                    placeholder="Email" 
-                    value={this.state.email}
-                    onChange={this.handleChange}/>
-                <input 
-                    type="tel" 
-                    id="phone" 
-                    placeholder="Phone Number" 
-                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
-                    value={this.state.phone}
-                    onChange={this.handleChange}/>
-                <input 
-                    type="text" 
-                    id="website" 
-                    placeholder="Website" 
-                    value={this.state.website}
-                    onChange={this.handleChange}/>
-                <button>Save</button>
+                <form onChange={this.handleChange}>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        placeholder="Name" 
+                        value={this.state.name}
+                        />
+                    <input 
+                        type="text" 
+                        id="location" 
+                        placeholder="Location" 
+                        value={this.state.location}
+                        />
+                    <input 
+                        type="email" 
+                        id="email" 
+                        placeholder="Email" 
+                        value={this.state.email}
+                        />
+                    <input 
+                        type="tel" 
+                        id="phone" 
+                        placeholder="Phone Number" 
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+                        value={this.state.phone}
+                        />
+                    <input 
+                        type="text" 
+                        id="website" 
+                        placeholder="Website" 
+                        value={this.state.website}
+                        />
                 </form>
                 </div>
-            </div>
-            <div style={{display: this.state.isEdit ? 'none' : 'block'}}>
-                <GeneralPreview data={this.state} />
-                <button onClick={this.handleSubmit}>Edit</button>
             </div>
         </div>
     )}
 }
-
-export default General;
