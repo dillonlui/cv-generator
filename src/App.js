c365f7ca5d134a09bb48ede0e4314609e3933815
Dillon Lui <dillonglui@gmail.com>
2021-05-14 10:58:15 -0500

    Reorganize file structure

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import General from "./components/General";
+import Education from "./components/Education";
+
+export default function App() {
+  return (
+    <div>
+        <General />
+    </div>
+  );
+}
