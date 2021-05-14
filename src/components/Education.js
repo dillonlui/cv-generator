14630b4d7d54b9cabd565222bbd0b0e952567cb9
Dillon Lui <dillonglui@gmail.com>
2021-05-14 16:27:25 -0500

    refactor code to add more education forms

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,83 +1,99 @@
 import React from "react";
-import EducationPreview from "./EducationPreview"
 
 export default class Education extends React.Component {
     constructor(){
         super()
         this.state={
-            numEdForm: 1,
-            school: '',
-            city: '',
-            degree: '',
-            schoolStart: '',
-            schoolEnd: '',
-            isEdit: true
+            educationInputs: [{
+                school: '',
+                city: '',
+                degree: '',
+                schoolStart: '',
+                schoolEnd: '',
+            }]
+            
         }
-        this.educationForm = this.educationForm.bind(this)
         this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.addEducation = this.addEducation.bind(this)
     }
 
     handleChange(e){
-        const { id, value} = e.target
-        this.setState({[id]: value})
+        let educationInputs = [...this.state.educationInputs]
+        educationInputs[e.target.dataset.id][e.target.className] = e.target.value
+        this.setState({educationInputs})
     }
-    handleSubmit(e){
-        e.preventDefault()
-        this.setState( {isEdit: !this.state.isEdit})
+    addEducation(e) {
+        this.setState((prevState) => ({
+            educationInputs: [...prevState.educationInputs, 
+                {school: '',
+                city: '',
+                degree: '',
+                schoolStart: '',
+                schoolEnd: '',
+                isEdit: true}
+        ]}))
     }
-
-    educationForm() {
-        return (
-            <div>
-                <div className="inputForm">
-                <form onSubmit={this.handleSubmit} style={{display: this.state.isEdit ? 'block' : 'none'}}>
-                    <input 
-                        type="text" 
-                        id="school" 
-                        placeholder="School" 
-                        onChange={this.handleChange}/>
-                    <input 
-                        type="text" 
-                        id="city" 
-                        placeholder="City" 
-                        onChange={this.handleChange}/>
-                    <input 
-                        type="text" 
-                        id="degree" 
-                        placeholder="Degree" 
-                        onChange={this.handleChange}/> 
-                        <input 
-                            type="text" 
-                            id="schoolStart" 
-                            placeholder="From"
-                            onChange={this.handleChange}/>
-                        <input 
-                            type="text" 
-                            id="schoolEnd"
-                            placeholder="To" 
-                            onChange={this.handleChange}/>
-                    <button>Save</button>
-                </form>
-                </div>
-                <div>
-                    <div style={{display: this.state.isEdit ? 'none' : 'block'}}>
-                        <EducationPreview data={this.state} />
-                    <button onClick={this.handleSubmit}>Edit</button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
 
     render() {
+        let {educationInputs} = this.state
         return (
             <div className="container">
                 <h2>Education</h2>
                 <hr />
-                {this.educationForm()}
+                <div>
+                    <button onClick={this.addEducation}>Add Education</button>
+                    {
+                        educationInputs.map((val, idx) => {
+                            let schoolId = `school-${idx}`,
+                                cityId = `city-${idx}`,
+                                degreeId = `degree-${idx}`,
+                                schoolStartId = `schoolStart-${idx}`,
+                                schoolEndId = `schoolEndId-${idx}`
+                            
+                            return( 
+                                <div key={idx}>
+                                <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+                                    <input 
+                                        type="text" 
+                                        id={schoolId}
+                                        data-id={idx}
+                                        placeholder="School"
+                                        className="school" 
+                                        />
+                                    <input 
+                                        type="text" 
+                                        id={cityId} 
+                                        data-id={idx}
+                                        placeholder="City" 
+                                        className="city"
+                                        />
+                                    <input 
+                                        type="text" 
+                                        id={degreeId} 
+                                        data-id={idx}
+                                        placeholder="Degree" 
+                                        className="degree"
+                                        /> 
+                                    <input 
+                                        type="text" 
+                                        id={schoolStartId} 
+                                        data-id={idx}
+                                        placeholder="From"
+                                        className="schoolStart"
+                                        />
+                                    <input 
+                                        type="text"                                         
+                                        id={schoolEndId}
+                                        data-id={idx}
+                                        placeholder="To" 
+                                        className="schoolEnd"
+                                        />
+                                </form>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
-        )
+        )}
     }
-}
